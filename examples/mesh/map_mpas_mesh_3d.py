6db23a37f7183b33936c0c043f9e2d5ed900539a
Chang Liao <20618384+changliao1025@users.noreply.github.com>
2026-03-06 22:21:37 -0700

    test

--- examples/mesh/map_mpas_mesh_3d.py
+++ examples/mesh/map_mpas_mesh_3d.py
@@ -5,8 +5,8 @@ from hexwatershed_utility.preprocess.mesh.map_mpas_mesh_on_sphere import map_mpa
 #sFilename_mpas_mesh_in = os.path.join('/compyfs/liao313/04model/pyhexwatershed/global/pyflowline20260203001/jigsaw/out','base_mesh.nc')
 #sFilename_png_out = os.path.join('/qfs/people/liao313/workspace/python/hexwatershed_utility/figures', 'mpas_mesh_ocn6_18coast6lnd12riv6_3d.jpg')
 
-sFilename_mpas_mesh_in = '/Users/liao313/scratch/04model/pyhexwatershed/global/pyflowline20260302001/base_mesh.nc'
-sFilename_png_out = '/Users/liao313/scratch/04model/pyhexwatershed/global/pyflowline20260302001/base_mesh.jpg'
+sFilename_mpas_mesh_in = '/Users/liao313/scratch/04model/pyhexwatershed/global/pyflowline20260302004/base_mesh.nc'
+sFilename_png_out = '/Users/liao313/scratch/04model/pyhexwatershed/global/pyflowline20260302004/base_mesh.jpg'
 
 sColormap = "terrain"
 # Test the culling fix with explicit parameters
